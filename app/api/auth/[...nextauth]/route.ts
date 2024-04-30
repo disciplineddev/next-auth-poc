21a7124b91b99a23455cedc9e1f24c8f0ed33e11
disciplineddev <disciplineddev1994@gmail.com>
2024-04-30 22:18:53 +0530

    Adding next auth configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,23 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-export function GET(req: NextRequest) {
-    return NextResponse.json({
-        message: 'Hellos'
-    })
-}
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          username: { label: 'email', type: 'text', placeholder: '' },
+          password: { label: 'password', type: 'password', placeholder: '' },
+        },
+        async authorize(credentials: any) {
+            
+            return {
+                id: "user1"
+            };
+        },
+      })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
+})
+
+export { handler as GET, handler as POST }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+"use client";
+import { signIn, signOut } from "next-auth/react"
 
 export default function Home() {
   return (
     <div>
-      Hello Next Hello world
-    </div>
+    <button onClick={() => signIn()}>Signin</button>
+    <button onClick={() => signOut()}>Sign out</button>
+  </div>
   );
 }
